20bc24dcec8079463a8289bc64a529fb9d79c286
Ilya Ivshin <58688997+korabelus@users.noreply.github.com>
2020-06-30 18:53:46 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,9 +5,14 @@ from random import randint
 
 if __name__ == '__main__':
 
-    target_list = [randint(10, 99) for i in range(20)]
-    print(target_list)
-    select_algorithm = BubbleSort(target_list)
-    sort_manager = SortManager(select_algorithm)
+    target_list1 = [randint(10, 99)for k in range(20)]
+    selected_alg = BubbleSort(target_list1)
+    sort_manager = SortManager(selected_alg)
     sort_manager.execute()
-    print(target_list)
+    print(target_list1)
+
+    target_list2 = [randint(10, 999)for k in range(20)]
+    selected_alg = SelectionSort(target_list2)
+    sort_manager = SortManager(selected_alg)
+    sort_manager.execute()
+    print(target_list2)
