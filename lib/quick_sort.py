98316b3eff56aa1930cf61f472914951734ee518
vlamik19 <madara020498@mail.ru>
2020-07-01 16:58:05 +0300

    Добавлен алгоритм Quick Sort в пакет lib

---
+++ lib/quick_sort.py
@@ -0,0 +1,21 @@
+import random
+from lib.abstract_strategy import SortAlgorithm
+
+
+class QuickSort(SortAlgorithm):
+
+    def __init__(self, target_list: list):
+        super().__init__(target_list)
+
+    def sort(self):
+        if len(self._target_list) <= 1:
+            return self._target_list
+        else:
+            q = random.choice(self._target_list)
+            l_arr = [x for x in self._target_list if x < q]
+            m_arr = [q] * self._target_list.count(q)
+            r_arr = [x for x in self._target_list if x > q]
+            return l_arr + m_arr + r_arr, self._target_list.sort()
+
+
+
